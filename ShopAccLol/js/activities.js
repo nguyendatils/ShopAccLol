f76658066c8415b63f60aa76d38b412f58a41251
nguyendatils <61770569+nguyendatils@users.noreply.github.com>
2020-10-17 20:27:23 +0700

    Add files via upload

---
+++ ShopAccLol/js/activities.js
@@ -0,0 +1,4 @@
+// show hidden info cua acc trong trang chu
+document.getElementsByClassName("prbox-1").onmouseover = function (){
+    document.getElementsByClassName("hidden-info").style.top = "0px";
+}
