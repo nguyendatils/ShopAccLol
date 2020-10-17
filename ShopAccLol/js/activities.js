29194ce0f8e1a9f87ac8b89010a124f98707594c
nguyendatils <61770569+nguyendatils@users.noreply.github.com>
2020-10-17 22:20:46 +0700

    update

--- ShopAccLol/js/activities.js
+++
@@ -1,4 +0,0 @@
-// show hidden info cua acc trong trang chu
-document.getElementsByClassName("prbox-1").onmouseover = function (){
-    document.getElementsByClassName("hidden-info").style.top = "0px";
-}
